406c83c325cd61fb6d699cf28846dbd10b121d9b
Robertsnights <ns433813@gmail.com>
2026-02-02 21:25:39 -0300

    terminar cabecera

--- src/cabecera.jsx
+++ src/cabecera.jsx
@@ -4,9 +4,22 @@ import './App.css'
 function Cuerpo1(){
 	return(
 		<>
-			<div className="Logo"><img src='./assets/imagenes/Logo.png' alt='' className="Logotipo"></img></div>
+			<div className="Logo"><div className="Logotipo"></div></div>
+			<div className="Nombre">
+				<h1><strong>Nicolas Agustin Sirtori</strong></h1>
+			</div>
+			<nav className="Botonera">
+				<ul>
+					<li className="menu"><a href={'#Cabecera'}>a</a></li>
+					<li className="menu"><a href={'#a'}>a</a></li>
+					<li className="menu"><a href={'#b'}>a</a></li>
+					<li className="menu"><a href={'#c'}>a</a></li>
+					<li className="menu"><a href={'#final'}>a</a></li>
+				</ul>
+			</nav>
 		</>
 	)
 }
+
 
 export default Cuerpo1
