fb9166fc1899ad76eccf0ed2e1535291fb6aec07
Robertsnights <ns433813@gmail.com>
2026-02-04 09:50:41 -0300

    Listado completo de cursos

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,16 @@ export function Cue1() {
     <>
       <div className="Foto"></div>
       <div className="Listados">
-        <DivText Titulo='Desarrollador de Videojuegos' Descripcion="Durante  años aprendi lo necesario para el desarrollo de videojuegos por medio de varios motores graficos."
+        <DivText Titulo='Desarrollador de Videojuegos' 
+          Descripcion="Durante  años aprendi lo necesario para el desarrollo de videojuegos por medio de varios motores graficos."
           anio="2020-2023"/>
-        <DivText  Titulo="Manifiesto"/>
-        <DivText  Titulo="Tarado"/>
+        <DivText  Titulo="Diplomatura Web con PHP"
+          Descripcion="Aprendi en durante los 3 cursos que duro el como programar en HTML y PHP. Añadir decorados con CSS y scrips JS"
+          anio="5-10 del 2025"/>
+        <DivText  Titulo="Curso de ReacJS y Lavrel"
+          Descripcion="Por medio de estos cursos obtuve los conocimientos para mejorar mis habilidades en el desarrollo web"
+          anio="durante 2026"
+        />
       </div>
     </>
   )
