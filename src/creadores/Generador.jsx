82ba977600e26708d37d383b46fda3f3e6213338
Robertsnights <ns433813@gmail.com>
2026-02-04 22:16:05 -0300

    por arreglar

--- src/creadores/Generador.jsx
+++ src/creadores/Generador.jsx
@@ -1,14 +1,34 @@
-import React, {useState} from 'react'
+import React, {use, useState} from 'react'
 export function Numeros(){
-	const [num, SetNumero] = useState(0)
-
+	const [num, setNumero] = useState(0)
+	const ListaImagenes =['foto1', 'foto2', 'foto3', 'foto4', 'foto5', 'foto6', 'foto7', 'foto8', 'foto9', 'foto10']
+	const [valores, Valor] = useState([])
 	const GenerarValor = ()=>{
 		const NuevoValor = Math.floor(Math.random()*10)+1;
-		SetNumero=NuevoValor;
+		setNumero(NuevoValor);
 	}
+	const Selecionardor = ()=>{
+		const regla = useState(0)
+		while (regla <8)  {
+			{GenerarValor}
+			const guardado=useState(0)
+			const prueba= Valor.includes(guardado)
+			if(({num}!= guardado)){
+				if(!prueba){
+					Valor.push(num)
+					guardado =num
+					regla = regla + 1
+				}
+			}
+		}
+	}
+
 	return(
 		<div>
-			<h4>{num}</h4>
+			{Selecionardor()}
+			<ul>
+				{valores.map((item, index)=> <li key={index}>{item}</li>)}
+			</ul>
 			<button onClick={GenerarValor}>Generar numero</button>
 		</div>
 	)
